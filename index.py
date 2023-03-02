86241454987f06eda09d6f86f79628d9dd82109f
koleso_O <kolesnikovfd@gmail.com>
2023-03-02 18:52:17 +0400

    Добавление капитана

--- index.py
+++ index.py
@@ -11,9 +11,13 @@ def main():
     db_session.global_init('db/blogs.db')
     db_sess = db_session.create_session()
     user = User()
-    user.name = "Пользователь 3"
-    user.about = "биография пользователя 3"
-    user.email = "email@email3.ru"
+    user.surname = 'Grisha'
+    user.name = 'Semenov'
+    user.age = 25
+    user.position = 'oldest helper'
+    user.speciality = 'scientist'
+    user.address = 'module_4'
+    user.email = 'grisha_banditos_228@mars.org'
     db_sess.add(user)
     db_sess.commit()
     '''
